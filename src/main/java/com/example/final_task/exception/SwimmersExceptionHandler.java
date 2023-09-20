d662d892449fa289b07e1b56b1ea09a65b4efafc
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-09-21 00:35:00 +0900

    改行、アノテーション修正

--- src/main/java/com/example/final_task/exception/SwimmersExceptionHandler.java
+++ src/main/java/com/example/final_task/exception/SwimmersExceptionHandler.java
@@ -3,14 +3,15 @@ package com.example.final_task.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZonedDateTime;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = ResourceNotFoundException.class)
+public class SwimmersExceptionHandler {
+    @ExceptionHandler(value = ResourceNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNoResourceFound(
             ResourceNotFoundException e, HttpServletRequest request) {
 
