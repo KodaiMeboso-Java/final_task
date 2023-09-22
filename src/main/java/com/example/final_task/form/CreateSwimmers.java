5398e9a4d5e2e7d71b7416d1c27cfb8d2cc9242f
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-09-22 15:03:38 +0900

    post2修正

--- src/main/java/com/example/final_task/form/CreateSwimmers.java
+++ src/main/java/com/example/final_task/form/CreateSwimmers.java
@@ -1,15 +1,12 @@
 package com.example.final_task.form;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateSwimmers {
-    @NotNull
-    private int id;
     @NotBlank
     private String name;
     @NotBlank
