33e6a13968d945fb15138c68c68c166134926d69
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-10-01 15:45:38 +0900

    unitTest-findByid

---
+++ src/test/java/com/example/final_task/service/SwimmersServicelmplTest.java
@@ -0,0 +1,49 @@
+package com.example.final_task.service;
+
+import com.example.final_task.entity.Swimmer;
+import com.example.final_task.mapper.SwimmersMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class SwimmersServicelmplTest {
+    @InjectMocks
+    SwimmersServiceImpl swimmersServicelmpl;
+    @Mock
+    SwimmersMapper swimmersMapper;
+
+    @Test
+    public void 存在する水泳選手がすべて取得できること() {
+        List<Swimmer> swimmerList = List.of(
+                new Swimmer(1, "meboso kodai", "breaststroke"),
+                new Swimmer(2, "ikee rikkako", "butterfly"),
+                new Swimmer(3, "irie ryosuke", "backstroke")
+        );
+        doReturn(swimmerList).when(swimmersMapper).findAll();
+        //テスト対象メソッドの呼び出し
+        List<Swimmer> actual = swimmersServicelmpl.findAll();
+        assertThat(actual).isEqualTo(swimmerList);
+        verify(swimmersMapper, times(1)).findAll();
+    }
+
+    @Test
+    public void 存在するIdを指定したときに水泳選手を取得できること() {
+        doReturn(Optional.of(new Swimmer(1, "meboso", "breaststroke"))).when(swimmersMapper).findById(1);
+
+        Swimmer actual = swimmersServicelmpl.findById(1);
+        assertThat(actual).isEqualTo(new Swimmer(1,"meboso","breaststroke"));
+        verify(swimmersMapper,times(1)).findById(1);
+    }
+}
