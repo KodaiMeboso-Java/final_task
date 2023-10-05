b00b802f950da54d4efecc95291faa6463c0c0cc
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-10-05 20:32:28 +0900

    create.OK

--- src/test/java/com/example/final_task/service/SwimmersServiceImplTest.java
+++ src/test/java/com/example/final_task/service/SwimmersServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.example.final_task.service;
 
 import com.example.final_task.entity.Swimmer;
+import com.example.final_task.form.CreateSwimmer;
 import com.example.final_task.mapper.SwimmersMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,10 +9,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -42,7 +46,16 @@ public class SwimmersServiceImplTest {
         doReturn(Optional.of(new Swimmer(1, "meboso", "breaststroke"))).when(swimmersMapper).findById(1);
 
         Swimmer actual = swimmersServiceImpl.findById(1);
-        assertThat(actual).isEqualTo(new Swimmer(1,"meboso","breaststroke"));
-        verify(swimmersMapper,times(1)).findById(1);
+        assertThat(actual).isEqualTo(new Swimmer(1, "meboso", "breaststroke"));
+        verify(swimmersMapper, times(1)).findById(1);
+    }
+
+    @Test
+    public void 水泳選手を登録できること() {
+        doNothing().when(swimmersMapper).create(any(Swimmer.class));
+
+        Swimmer swimmer = new Swimmer("seto daiya", "IM");
+        swimmersServiceImpl.create("seto daiya", "IM");
+        verify(swimmersMapper).create(any(Swimmer.class));
     }
 }
